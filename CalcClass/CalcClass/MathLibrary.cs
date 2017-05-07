a3a1908c5c04013690c69bd2711af2c225a55d5e
Yurii <yvients@gmail.com>
2017-05-07 13:12:29 +0300

    Add library CalcClass

---
+++ CalcClass/CalcClass/MathLibrary.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcClass
+{
+    public class MathLibrary
+    {
+        private static string _lastError = "";
+        public static string lastError
+        {
+            get { return _lastError; }
+            set { _lastError = value; }
+        }
+
+        public static int Add(long a, long b)
+        {
+            int result;
+            try
+            {
+                result = (int)(a + b);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            return result;
+        }
+        public static int Sub(long a, long b)
+        {
+            int result;
+            try
+            {
+                result = (int)(a - b);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            return result;
+        }
+
+
+        public static int Mult(long a, long b)
+        {
+            int result;
+            try
+            {
+                result = (int)(a * b);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            return result;
+        }
+
+        public static int Div(long a, long b)
+        {
+            int result;
+            try
+            {
+                result = (int)(a / b);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            catch (DivideByZeroException)
+            {
+                _lastError = "Error 09";
+                result = 0;
+            }
+            return result;
+        }
+        public static int Mod(long a, long b)
+        {
+            int result;
+            try
+            {
+                result = (int)(a % b);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            catch (DivideByZeroException)
+            {
+                _lastError = "Error 09";
+                result = 0;
+            }
+            return result;
+        }
+      
+        public static int ABS(long a)
+        {
+            int result;
+            try
+            {
+                if (a > 0) return (int)(a);
+                else return (int)(-a);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            return result;
+        }
+        public static int IABS(long a)
+        {
+            int result;
+            try
+            {
+                if (a > 0) return (int)(-a);
+                else return (int)(a);
+            }
+            catch (OverflowException)
+            {
+                _lastError = "Error 06";
+                result = 0;
+            }
+            return result;
+        }
+    }
+}
